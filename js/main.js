da836c6518eedf4e4a4fbbbee05425eab0ba171f
Yoon Hwan Cho <yhcho0714@gmail.com>
2022-07-19 21:48:17 +0000

    Completed Add an anchor to the entries

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-var photoURL = document.querySelector('#photo-url');
-photoURL.addEventListener('input', displayImage);
+var getPhotoURL = document.querySelector('#photo-url');
+getPhotoURL.addEventListener('input', displayImage);
 var image = document.querySelector('img');
 
 function displayImage(event) {
@@ -13,6 +13,7 @@ function submitEntry(event) {
   event.preventDefault();
   var entryObj = {};
 
+  // add  .value at the end of title photoUrl notes
   entryObj.title = formEntry.elements.title.value;
   entryObj.photoUrl = formEntry.elements['photo-url'].value;
   entryObj.notes = formEntry.elements.notes.value;
@@ -21,36 +22,57 @@ function submitEntry(event) {
   data.entries.unshift(entryObj);
   image.setAttribute('src', 'images/placeholder-image-square.jpg');
   formEntry.reset();
+
 }
 
 // need to add entry as parameter
-// function renderList(entry) {
-//   var liEl = document.createElement('li');
-//   liEl.setAttribute('class', 'row margin-b40');
+function renderList(entry) {
+  var liEl = document.createElement('li');
+  liEl.setAttribute('class', 'row margin-b40 align-center');
 
-//   var divImg = document.createElement('div');
-//   divImg.setAttribute('class', 'column-full column-half');
-//   liEl.appendChild(divImg);
+  var divImg = document.createElement('div');
+  divImg.setAttribute('class', 'column-full column-half');
+  liEl.appendChild(divImg);
 
-//   var viewImg = document.createElement('img');
-//   //  this will be the data.entries[i].photoUrl the loop--
-//   viewImg.setAttribute('src', 'https://www.planetware.com/wpimages/2019/06/south-korea-top-tourist-attractions-itaewon.jpg');
-//   divImg.appendChild(viewImg);
+  var viewImg = document.createElement('img');
+  viewImg.setAttribute('src', entry.photoUrl);
+  divImg.appendChild(viewImg);
 
-//   var divText = document.createElement('div');
-//   divText.setAttribute('class', 'column-full column-half');
-//   liEl.appendChild(divText);
+  var divText = document.createElement('div');
+  divText.setAttribute('class', 'column-full column-half');
+  liEl.appendChild(divText);
 
-//   var h3Title = document.createElement('h3');
-//   //  this will be the data.entries[i]. the loop--
-//   h3Title.textContent = data.entries[5].title;
-//   divText.appendChild(h3Title);
+  var h3Title = document.createElement('h3');
+  h3Title.textContent = entry.title;
+  divText.appendChild(h3Title);
 
-//   var paraNotes = document.createElement('p');
-//   //  this will be the data.entries[i]. the loop--
-//   paraNotes.textContent = data.entries[5].notes;
-//   divText.appendChild(paraNotes);
+  var paraNotes = document.createElement('p');
+  paraNotes.textContent = entry.notes;
+  divText.appendChild(paraNotes);
 
-//   console.log('liEl: ', liEl);
+  return liEl;
+}
 
-// }
+var ulEl = document.querySelector('.list-none');
+window.addEventListener('DOMContentLoaded', loadView);
+function loadView() {
+  for (var i = data.entries.length - 1; i >= 0; i--) {
+    ulEl.prepend(renderList(data.entries[i]));
+  }
+}
+
+var viewEntries = document.querySelector('.entry-view');
+
+viewEntries.addEventListener('click', hideNewShowEntries);
+
+function hideNewShowEntries(event) {
+  /*   var formDataValue = event.target.getAttribute('data-view');
+  console.log(formDataValue); */
+
+  var $getForm = document.querySelector('.get-form');
+  $getForm.className = 'hidden';
+  /*   console.log('$getForm now hides', $getForm); */
+
+  var $getEntries = document.querySelector('.get-entries');
+  $getEntries.className = 'get-entries';
+}
