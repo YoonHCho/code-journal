c3c7227b1c576b94939483387a8ccefb1309e13e
Yoon Hwan Cho <yhcho0714@gmail.com>
2022-07-19 06:03:19 +0000

    defined a function with most of journal entry, need to uncomment

--- js/main.js
+++ js/main.js
@@ -22,3 +22,35 @@ function submitEntry(event) {
   image.setAttribute('src', 'images/placeholder-image-square.jpg');
   formEntry.reset();
 }
+
+// need to add entry as parameter
+// function renderList(entry) {
+//   var liEl = document.createElement('li');
+//   liEl.setAttribute('class', 'row margin-b40');
+
+//   var divImg = document.createElement('div');
+//   divImg.setAttribute('class', 'column-full column-half');
+//   liEl.appendChild(divImg);
+
+//   var viewImg = document.createElement('img');
+//   //  this will be the data.entries[i].photoUrl the loop--
+//   viewImg.setAttribute('src', 'https://www.planetware.com/wpimages/2019/06/south-korea-top-tourist-attractions-itaewon.jpg');
+//   divImg.appendChild(viewImg);
+
+//   var divText = document.createElement('div');
+//   divText.setAttribute('class', 'column-full column-half');
+//   liEl.appendChild(divText);
+
+//   var h3Title = document.createElement('h3');
+//   //  this will be the data.entries[i]. the loop--
+//   h3Title.textContent = data.entries[5].title;
+//   divText.appendChild(h3Title);
+
+//   var paraNotes = document.createElement('p');
+//   //  this will be the data.entries[i]. the loop--
+//   paraNotes.textContent = data.entries[5].notes;
+//   divText.appendChild(paraNotes);
+
+//   console.log('liEl: ', liEl);
+
+// }
